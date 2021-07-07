33501c3e183daaa9595176949aae6d9adf2b41c9
seung-h <dau4723@gmail.com>
2021-07-07 22:00:01 +0900

    todo

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
   </div>
 </template>
 
@@ -13,6 +13,32 @@ import TodoFooter from './components/TodoFooter.vue'
 import TodoList from './components/TodoList.vue'
 import TodoInput from './components/TodoInput.vue'
 export default {
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  methods: {
+    addTodo(todoItem) {
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  },
+  created() {
+    if (localStorage.length > 0) {
+        for (var i=0;i<localStorage.length;i++) {
+            this.todoItems.push(localStorage.key(i));
+        }
+    }
+  },
   components: {
     'TodoHeader': TodoHeader,
     'TodoInput': TodoInput,
